ff31ada96e7ec16ad96423b44c2d58587fbf779c
LorenBaker <LorenABaker@comcast.net>
2018-12-01 12:15:38 -0800

    Static Layout

--- src/features/event/EventList/EventListAttendee.jsx
+++ src/features/event/EventList/EventListAttendee.jsx
@@ -3,13 +3,13 @@ import { List, Image } from "semantic-ui-react";
 
 class EventListAttendee extends Component {
   render() {
+    const { attendee } = this.props;
     return (
       <List.Item>
-        <div>ATTENDEE</div>
-        <Image as ='a' size='mini' circular scr='https://randomuser.me/api/portraits/women/42.jpg'/>
+        <Image as="a" size="mini" circular src={attendee.photoURL} />
       </List.Item>
-    )
+    );
   }
 }
 
-export default EventListAttendee
+export default EventListAttendee;
